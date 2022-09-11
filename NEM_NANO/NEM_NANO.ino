13c3c6ff1b217bc2a516c21633a83c04b3901d56
meltoner <kp@rei.gr>
2022-09-12 01:01:29 +0300

    Add files via upload

--- NEM_NANO/NEM_NANO.ino
+++ NEM_NANO/NEM_NANO.ino
@@ -1,40 +1,57 @@
 // @author : Konstantinos Papageorgiou - 2022 - kp at rei.gr
 // @repo: https://github.com/meltoner/NEM_NANO
 //
-// # NEM NANO DISTANCE
-// 
-// NEM GPS FLYSKY GPS DISTANCE, Voltage SENSOR on Arduino nano
-// 
-// Derives and transmits the distance from home, voltage, gps coordinates, gps sattelite, speed to the flysky remote controller screen using an arduino nano 
-// 
-// ## Software Features :
-// 
-// - Software utilities a GPS, remote control on an arduino nano
-// - Sensors and actuators are all in classes
-// - Implements five tasks execution frequencies
-// - Auto derives home after 10 seconds since first lock, and after seeing at least 6 satellites
-// - Derives distance and degrees from a target gps LAT LOT
-// - Transmits distance, speed, voltage, gps coordinates using two number per coordinate
-// - Led indicator low periodicity no gps lock, high periodicity gps lock
-// - doesn't occupy a serial port to transmit telemetry
-// 
-// ## Hardware
-// 
-// Sensors :
-// 
-// - Gps : Ublox M8N with compass
-// - Voltage sensor 0-25v MH-electronic
-// - Receiver : flysky F9-ia10b
-// 
-// Main components : 
-// - Transmitter : flysky FS-I6X
-// - Receiver : [flysky FS-1A10B](https://www.flysky-cn.com/ia10b-canshu)
-// - Arduino nano
-// 
-// Other: 
-// 
-// - LED
-// - 3 Pin Female connectors, ribbon cable, conectors, board, box, battery, etc
+//  # NEM NANO 
+//  
+//  NEM GPS FLYSKY DISTANCE, Voltage, speed sensor on Arduino nano
+//  
+//  The component, derives and transmits the distance from home, the external voltage, the gps coordinates, the gps satellites, the speed 10cm/sec, to the flysky //  remote controller (flysky FS-I6X) screen, using an Arduino nano. Built using a custom pcb and software designed, for http://nem.gr by Konstantinos L. Papageorgiou.
+//  
+//  ## Video showcase
+//  
+//  - https://www.youtube.com/watch?v=HKdX1mCnNCI
+//  
+//  ## Software Features :
+//  
+//  - Software utilities a GPS, remote control on an arduino nano
+//  - Sensors and actuators are all in classes
+//  - Implements five tasks execution frequencies
+//  - Auto derives home after 10 seconds since first lock, and after seeing at least 6 satellites
+//  - Derives distance and degrees from a target gps LAT LOT
+//  - Transmits distance from home
+//  - Transmits speed in 10cm/secon
+//  - Transmits External Voltage,
+//  - Transmits gps coordinates using two numbers per coordinate
+//  - Led indicator low periodicity no gps lock, high periodicity gps lock
+//  - Does not occupy a serial port to transmit telemetry data
+//  
+//  ## Hardware
+//  
+//  Sensors :
+//  
+//  - Gps : Ublox M8N with compass
+//  - Voltage sensor 0-25v MH-electronic
+//  - Receiver : flysky F9-ia10b
+//  
+//  - Transmitter : flysky FS-I6X
+//  - Receiver : [flysky FS-1A10B](https://www.flysky-cn.com/ia10b-canshu)
+//  - Arduino nano
+//  
+//  - LED
+//  - 3 Pin Female connectors, ribbon cable, conectors, board, box, battery, etc
+//  
+//  ## Connectivity
+//  
+//  - https://github.com/meltoner/NEM_NANO/blob/main/supportive/nem_nano_connections.pdf
+//  
+//  ## Images
+//  
+//  - https://github.com/meltoner/NEM_NANO/tree/main/media
+//  
+//  ## PCB board 
+//  
+//  - https://github.com/meltoner/NEM_NANO/tree/main/pcb_and_box
+//  
 
 
 #include "Context.h"
