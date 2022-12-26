6ecab32752e1fe68991de694c3bf1a2502c18cb8
meltoner <kp@rei.gr>
2022-12-26 11:31:22 +0200

    cell battery resolution and capacity derivation

--- NEM_NANO/Battery.cpp
+++ NEM_NANO/Battery.cpp
@@ -1,5 +1,5 @@
 /*
-  Battery.h - Library for wraping the functions controling the Battery power levels
+  Battery.h - Library for warping the functions controlling the Battery power levels
   Author - Konstantinos Papageorgiou kp at rei.gr 2022 
 */
 
@@ -12,17 +12,24 @@ Battery::Battery(byte pin){}
 void Battery::setup(Context &_context){
   context = &_context;
   apply();
+  measureVoltage();
+  // battery cell detection based on single cell min (3.54), max (4.2) multiples
+  for(cells = 1;cells < 6;cells++)
+    if((cells * minV) > context->voltage ){
+      cells--;
+      break;
+    }
+}
+
+float Battery::measureVoltage(){
+  context->voltage =  analogRead(0) / 40.92;
+  context->voltage = context->voltage + 0.5;
+  return context->voltage;
 }
 
 void Battery::apply(){
-  context->voltage =  analogRead(A7) / 40.92; 
-  // abstract to % based on range 6v to 7.4v    
-  context->capacity = (byte)(context->voltage < 6 ? 0:(context->voltage - 6) * 71.428);
-  if(context->capacity > 100)
-    context->capacity = 100;
-
-  context->isLowBattery = context->capacity < 40;
-  //if(context->isLowBattery)
-  //  context->intervals[5] = 60;
-
+  measureVoltage();
+  // derive how close we are to the min voltage based on the number of cell
+  context->capacity = (byte)( ((context->voltage - (cells * minV)) / (context->voltage - (cells * maxV))) * 100 ); 
+  context->isLowBattery = context->voltage < (cells * minV);
 }
--- NEM_NANO/Battery.h
+++ NEM_NANO/Battery.h
@@ -1,7 +1,8 @@
 /*
-  Battery.h - Library for wraping the functions controling the Battery power levels
+  Battery.h - Library for warping the functions relevant to the Battery power levels
   Author - Konstantinos Papageorgiou kp at rei.gr 2022 
 */
+
 #ifndef Battery_h
 #define Battery_h
 
@@ -13,8 +14,23 @@ class Battery{
     Battery(byte pin);
     void setup(Context &_context);
     void apply();
+    float measureVoltage();
   private:
+
+    byte cells = 1;
+    float minV = 3.54;
+    float maxV = 4.2;
     Context *context;
 };
 
 #endif
+
+// cell min   max
+// 1    3.54  4.2
+// 2    7.08  8.4
+// 3    10.62 12.6
+// 4    14.16 16.8
+// 5    17.7  21
+// 6    21.24 25.2
+// 7    24.78 29.4
+// 8    28.32 33.6
